b3b4308f2b5fda44bd1ca1e1be644d2ebcc097e7
TECH VALLEY <TECH VALLEY@DESKTOP-CUMGKU2>
2024-10-19 22:36:11 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var user01 = new User(1, "Islam", "Manager");
+
+            var user02 = new User(2, "Ahmed", "Employee");
+
+            var user03 = new User(3, "Fawaz", "TL");
+
+
+
+            Console.WriteLine(user01.ToString());
+            Console.WriteLine(user02.ToString());
+            Console.WriteLine(user03.ToString());
+
+
+        }
+
+
+    }
+}
